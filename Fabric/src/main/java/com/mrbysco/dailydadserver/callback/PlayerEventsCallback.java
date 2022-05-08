4c9a59b81ea5b0d09a1224589f8d0cbd159da6e1
Mrbysco <Mrbysco@users.noreply.github.com>
2022-05-08 19:49:54 +0200

    Update forge/fabric
    Update cloth/modmenu

--- Fabric/src/main/java/com/mrbysco/dailydadserver/callback/PlayerEventsCallback.java
+++ Fabric/src/main/java/com/mrbysco/dailydadserver/callback/PlayerEventsCallback.java
@@ -6,37 +6,33 @@ import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
 
 public interface PlayerEventsCallback {
-	Event<Login> LOGIN_EVENT = EventFactory.createArrayBacked(Login.class,
-			(listeners) -> (player) -> {
-				for (Login event : listeners) {
-					InteractionResult result = event.onLogin(player);
+	Event<Login> LOGIN_EVENT = EventFactory.createArrayBacked(Login.class, (listeners) -> (player) -> {
+		for (Login event : listeners) {
+			InteractionResult result = event.onLogin(player);
 
-					if (result != InteractionResult.PASS) {
-						return result;
-					}
-				}
-
-				return InteractionResult.PASS;
+			if (result != InteractionResult.PASS) {
+				return result;
 			}
-	);
+		}
+
+		return InteractionResult.PASS;
+	});
 
 	interface Login {
 		InteractionResult onLogin(Player player);
 	}
 
-	Event<Respawn> RESPAWN_EVENT = EventFactory.createArrayBacked(Respawn.class,
-			(listeners) -> (player, endConquered) -> {
-				for (Respawn event : listeners) {
-					InteractionResult result = event.respawn(player, endConquered);
+	Event<Respawn> RESPAWN_EVENT = EventFactory.createArrayBacked(Respawn.class, (listeners) -> (player, endConquered) -> {
+		for (Respawn event : listeners) {
+			InteractionResult result = event.respawn(player, endConquered);
 
-					if (result != InteractionResult.PASS) {
-						return result;
-					}
-				}
-
-				return InteractionResult.PASS;
+			if (result != InteractionResult.PASS) {
+				return result;
 			}
-	);
+		}
+
+		return InteractionResult.PASS;
+	});
 
 	interface Respawn {
 		InteractionResult respawn(Player player, boolean endConquered);
